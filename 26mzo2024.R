b529eb9b1ea91a2a33e5967f09dedd6ce30d9b61
Angel Gomez <angomalva@gmail.com>
2024-03-26 15:54:05 -0600

    otro

--- 26mzo2024.R
+++ 26mzo2024.R
@@ -5,6 +5,25 @@
 #lubridate
 #glue
 #tsibble time series
+#tidytext text mining 
+#tidyr
+#plotly
+#purr
+#stringr
+#forcats
+#knitr::kable
+#shiny
+#reticulate use python with R
+
+
+
+
+#ml3
+#flexdashboard
+#data.table
+
+#caret
+
 
 
 
