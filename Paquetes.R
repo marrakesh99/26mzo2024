ab7381d043b5b13a48a48ae8c9e88b0ec3492a22
Angel Gomez <angomalva@gmail.com>
2024-03-26 16:52:39 -0600

    cambio de nombre de 26mzo a paquetes.R

---
+++ Paquetes.R
@@ -0,0 +1,55 @@
+#import
+  #vroom
+  #foreign
+  #haven
+  #readxl
+  #tidyxl
+  #httr
+  #rvest. Web scraping
+  #data.table
+
+#tidylog
+#janitor
+#lubridate
+#glue
+#tidytext text mining 
+#tidyr
+#plotly
+#purr
+#stringr
+#forcats
+
+#sqldf
+
+
+#tsibble time series
+#zoo
+
+
+#shiny
+#flexdashboard
+
+
+
+#DataExplorer
+#dlookr diagnose, explore and repair your data quick
+
+#ml3. Machine learning models. 
+#xgboost. gradient boosting framework. 
+#caret. Classification and regression
+#e1071. Clustering, Fourier Transform, Naive Bayes, SVM
+#h20
+
+
+
+
+
+
+
+#knitr. Reports
+#knitr::kable
+
+#xml. To read and create xml documents
+#jsonlite
+
+#reticulate use python with R
