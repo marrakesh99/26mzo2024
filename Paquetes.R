5f5dcb96338d72ea522e4ae9b0c216c6cae20fa0
Angel Gomez <angomalva@gmail.com>
2024-03-26 18:34:38 -0600

    mas packages

--- Paquetes.R
+++ Paquetes.R
@@ -1,3 +1,7 @@
+#------------------------------------------------
+#good source of packages
+#https://roelverbelen.netlify.app/resources/r/packages/
+#-------------------------------------------------------
 #import
   #vroom
   #foreign
@@ -7,6 +11,20 @@
   #httr
   #rvest. Web scraping
   #data.table
+  #arrow. Parquet
+  #readr
+  #rio. Pull a lot of separate data-reading packages into one, so you just need 
+#to remember 2 functions: import and export.
+  #rcrawler. web scrapping
+  #openxlsx
+  #googlesheets
+  #git2r
+  #DBI
+  #feather. Fast on disk format powered by apache arrow.
+
+
+#----------------------
+#Data Wrangling and exploration
 
 #tidylog
 #janitor
@@ -18,38 +36,120 @@
 #purr
 #stringr
 #forcats
+#sqldf, Use Sql
+#diffobj. Similar to identical() but with a visual representation
+#dbplyr. Connect to a database
+#DataeditR. Data editor spreadsheet style
+#pivottabler. Create pivot tables
+#fuzzyjoin
+#tidyquery
+#skimr. handles summary statistics
+#naniar. Dealing with NAs, also graphical representation
 
-#sqldf
-
-
-#tsibble time series
-#zoo
-
-
-#shiny
-#flexdashboard
-
-
+#scales. Format data for graphic. It's worth for comma(), percent() and 
+#dollar() functions
 
 #DataExplorer
 #dlookr diagnose, explore and repair your data quick
+#visdat. Visualize a dataframe.
+#daff. check the difference, patch and merge dataframes
 
+
+-------------------------------
+# Tables and charts
+
+# ggplot2
+# plotly
+# lattice
+#rbokeh
+  
+# esquisse. Drag and drop interface for ggplot2
+# dygraphs. Html/Javascript graphs of timeseries
+#RColorBrewer. Select color palettes for your visualizations
+#sf. GIS work in R
+#leaflet. Maps
+# DT
+# reactable  
+#kableExtra
+#gt
+#gtsummary
+#patchwork. Generate a grid for many charts or put one over another
+#flextable
+
+
+#---------------
+#Extensions of ggplot
+#ggraph. Overrides the dependency of ggplot on tabular data
+#ggmap
+#ggrepel. Prevent plot labels from overlapping
+#gganimate
+#ggforce
+#
+
+
+#--------------
+
+#Time Series
+#tsibble time series format
+#zoo. functions for dealing with time series data
+#tsbox. Convert data between different R time series data formats
+#tseries
+#prophet. Forecasting time series.
+#tidyverts
+#modeltime
+
+#-------------------------------------------
+
+
+
+
+
+#----------------------------
+#Models and Machine Learning
+
+#broom. complements stats by tidying the output of various statistical models.
+#MASS. statistical functions
 #ml3. Machine learning models. 
 #xgboost. gradient boosting framework. 
 #caret. Classification and regression
 #e1071. Clustering, Fourier Transform, Naive Bayes, SVM
 #h20
+#tidymodels
+#tensorflow
+#tidyquant
+#rocr. Evaluating and visualizing the performance of classification models.
+#glmnet. Regression models with regularization techniques like lasso and elastic net
+#randomForest
+#tm. text mining
+#forecast
 
 
 
-
-
-
+#-----------------------------
+#Reports and dashboards
 
 #knitr. Reports
 #knitr::kable
+#rmarkdown
+#shiny
+#flexdashboard
 
+#------------------------------
+
+#Other file formats
 #xml. To read and create xml documents
 #jsonlite
 
+
+
+#-----------------------------------
+
+#Interaction with other languages
 #reticulate use python with R
+#plumber. Connect r code with other languages
+
+#------------------------------------
+Big data
+#sparklyr. Distributed computing
+#dask. to work with larger than memory datasets
+
